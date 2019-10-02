eb26611ffd2df516b5dae0e8638852120405bc63
Alex Harry <alex.harry1@gmail.com>
2019-10-01 20:45:34 -0600

    Added a test to deal with conflict of knn

--- test.py
+++ test.py
@@ -8,7 +8,13 @@ class Test(unittest.TestCase):
     def test_predict_by_distance(self):
         knn = KNN()
         class_list = ['right_pick','wrong_pick', 'wrong', 'right_pick', 'wrong_again']
-        self.assertTrue(knn.predict_by_distance(class_list), 'right_pick')
+        self.assertEqual(knn.predict_by_distance(class_list), 'right_pick')
+
+    def test_predict_by_distance_with_conflict(self):
+        knn = KNN()
+        class_list = ['wrong_pick','right_pick', 'wrong_pick', 'wrong', 'right_pick', 'wrong_again']
+        self.assertEqual(knn.predict_by_distance(class_list), 'right_pick')
+
 
 
 # Source to understand how to test in python: https://pymbook.readthedocs.io/en/latest/testing.html and https://docs.python.org/2/library/unittest.html
