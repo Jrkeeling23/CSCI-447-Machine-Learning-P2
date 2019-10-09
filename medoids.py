1d927b61781ecfb0e79bdb73be7fc494cb8711bc
Jrkeeling23 <jrkeeling23@gmail.com>
2019-10-08 21:24:33 -0600

    optimized the functions in medoids file

--- medoids.py
+++ medoids.py
@@ -3,11 +3,28 @@ import pandas as pd
 import random
 
 
+def euclidean_distance(query_point, comparison_point, df):
+    """
+    Performs the Euclidean distance function
+    :param query_point: a data point
+    :param comparison_point: a comparison point
+    :param df: used to get the label columns
+    :return: a distance
+    """
+    temp_add = 0  # (x2-x1)^2 + (y2 - y1)^2 ; addition part
+    for feature_col in range(len(query_point)):
+        if df.get_label_col(df.data_name) is feature_col:
+            continue
+        if type(query_point[feature_col]) is float or type(query_point[feature_col]) is int:
+            temp_sub = (query_point[feature_col] - comparison_point.row[feature_col]) ** 2  # x2 -x1 and square
+            temp_add += temp_sub  # continuously add until square root
+
+    return temp_add ** (1 / 2)  # square root ... return the specific distance
+
 class KMedoids:
 
     def __init__(self, data):
         self.df = data
-        self.medoids = None
         self.medoids_list = None  # contains a list of the Medoid instances
         self.data_name = None
 
@@ -18,10 +35,9 @@ class KMedoids:
         :param data_name:
         :return:
         """
-        self.data_name = data_name
-        self.medoids = self.select_random(k)
-        self.create_medoid_instances(self.medoids)
-        self.assign_to_medoids()
+        self.data_name = data_name  # assigns the current data set being used for label column purposes
+        self.select_random(k)  # select random data points to represent the medoids
+        self.assign_to_medoids()  # assign the remaining data points to its closest medoid
 
     def select_random(self, k):
         """
@@ -29,46 +45,30 @@ class KMedoids:
         :param k: number of medoids
         :return: k random data points
         """
-        return self.df.sample(n=k)
+        self.create_medoid_instances(self.df.sample(n=k))
 
     def assign_to_medoids(self):
         """
-
+        assigns the remaining data points to its closest medoid
         :return:
         """
         for index, row in self.df.iterrows():
-            if index in self.medoids.index:
-                continue
+            if index in self.medoids_list:
+                continue  # exclude to data points that are medoids
             else:
-                self.check_all_medoid_distances(row)  # checking the row to all medoids for closest distance
-
-        return True
+                chosen_medoid = self.check_all_medoid_distances(row)  # check the row to  medoids for closest distance
+                chosen_medoid.encompasses.assign_to_medoid(index)  # assign the index of the data point to the medoid
 
     def check_all_medoid_distances(self, query_point):
         """
-
-        :return:
+        calculate the Euclidean distances from the query point to the medoid
+        :param query_point:
+        :return:  distance
         """
-        distance_dict = {}
-        for index, row in self.medoids.iterrows():
-            distance_dict[index] = self.euclidean_distance(query_point, row)  # add the distances to the dict with index
-
-    def euclidean_distance(self, query_point, comparison_point):
-        """
-        With multi dimensions: sqrt((x2-x1)+(y2-y1)+(z2-z1)+...))
-        :param query_point: an example from the data set
-        :param comparison_point: a medoid
-        :return: float distance
-        """
-        temp_add = 0  # (x2-x1)^2 + (y2 - y1)^2 ; addition part
-        for feature_col in range(len(query_point)):
-            if self.df.get_label_col(self.data_name) is feature_col:
-                continue
-            if type(query_point[feature_col]) is float or type(query_point[feature_col]) is int:
-                temp_sub = (query_point[feature_col] - comparison_point[feature_col]) ** 2  # x2 -x1 and square
-                temp_add += temp_sub  # continuously add until square root
-
-        return temp_add ** (1 / 2)  # square root
+        distances = {}  # dict to hold the indexes and the distances
+        for med in self.medoids_list:
+            distances[med] = euclidean_distance(query_point, med.row, self.df)
+        return self.sort_dict_by_value(distances)
 
     def sort_dict_by_value(self, sort_this):
         """
@@ -76,7 +76,7 @@ class KMedoids:
         :param sort_this: dictionary of distances from query point to medoids
         :return: closest medoid
         """
-        k_size = self.medoids.shape[0]
+        k_size = len(self.medoids_list)
         first_iteration = True
         closest = None
         for k, v in sort_this.items():
@@ -87,7 +87,7 @@ class KMedoids:
             else:
                 if closest > v:
                     closest = k  # change closest var to closer
-        return closest
+        return closest  # closest medoid
 
     def create_medoid_instances(self, medoids):
         """
@@ -109,14 +109,15 @@ class Medoids:
         :param index: index of the point
         """
         self.medoid_point = point
-        self.encompasses = {}
+        self.encompasses = []
         self.index = index
 
-    def assign_to_medoid(self, index, row):
+    def assign_to_medoid(self, index):
         """
         Assigns data to the medoid
         :param index: index of data
-        :param row:  row of data  ... may not be needed
         :return:
         """
-        self.encompasses[index] = row
+        self.encompasses.append(index)
+
+    # TODO: add functions to change the medoids and pick a more appropraite point
