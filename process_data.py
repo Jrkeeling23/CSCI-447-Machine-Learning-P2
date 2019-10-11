f509585944cdf8b9e52b8b3fc384a7d96860b6b5
TheGreatFatCat <thefatcatproductons@gmail.com>
2019-10-10 20:58:51 -0600

    Setup K fold to split data into list ok k lists containg test and training set

--- process_data.py
+++ process_data.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import csv
-from KNN import KNN
 
 
 class Data:
@@ -67,36 +66,36 @@ class Data:
 
 
 
-    def k_fold_KNN(self, split_data, k_val, name):
-        """
-             perform k fold on KNN with split dataset
-             :param split data: list of k split dataset
-             :param k_val: k we used when splitting data
-             :param name: dataset name
-             :param knn_k: number of neighbors
-             :param
-             :return:
-             """
-        i = 0
-        usedSetIndex = 0;  # keep track of how many datasets out of K we have used
-        while i < len(split_data):
-            test_set = split_data[i]
-            split_data.remove(split_data[i])  # remove test set from list of current data
-            train_set = pd.concat(split_data) # create new training set
-            KNN
-
-
-
 
 
     def split_k_fold(self, k_val, dataset):
         """
         Split data into list of K different parts
         :param k_val: k value to set size of folds.
-        :return: list of k different data sets to be used for test / training
+        :return: list of lists where arranged as follows [[train,test], [train, test]] repeated k times
+        where train is traing data (index 0) and test is testing data (index 1)
         """
         k__split_data = np.array_split(dataset, k_val) # splits dataset into k parts
-        return k__split_data
+        # now we need to split up data into 1 list and k others combined into 1 list for test/train
+
+        test_train_sets = []
+        temp_list = [None] * 2
+        length = len(k__split_data)
+
+
+        # create these new lists and add them to test_train_sets
+
+        for i in range(length):  # go through every split list
+            # APPARENTLY PYTHON DEVS THOUGHT IT WAS A GOOD FUCKING IDEA TO MAKE LISTS THAT HAVE DIFFERENT NAMES BOTH
+            # REMOVE VALS WHEN THE REMOVE FUNCTION IS APPLIED TO ONE OF THEM.   WHY GOD WHY
+            data_to_combine = np.array_split(dataset, k_val)
+            temp_list[0] = k__split_data[i]
+           # del data_to_combine[i]
+            temp_list[1] = pd.concat(data_to_combine)
+            test_train_sets.append(temp_list)
+            i += 1
+
+        return test_train_sets
 
 
 
